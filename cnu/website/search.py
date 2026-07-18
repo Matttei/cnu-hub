0c1d2731499c514099f5e263c1bdd562e59d5dbd
Dorcea Matei <dorceamatei010@gmail.com>
2026-07-18 17:48:58 +0300

    search fix

--- cnu/website/search.py
+++ cnu/website/search.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.urls import reverse
 import unicodedata
 import re
-BASE_URL = "https://cnu-hub.onrender.com/"
+BASE_URL = "https://cnu-hub.onrender.com"
 
 def normalize(text):
     text = unicodedata.normalize("NFD", text)
